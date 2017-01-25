464c16f531d57e54a85479d116cbd7b8d353203a
kspringall <kimballg@springall.name>
2017-01-25 15:54:15 -0700

    Added smoketest for greeting controller

--- src/main/java/com/kspringall/pi4led/controller/GreetingController.java
+++ src/main/java/com/kspringall/pi4led/controller/GreetingController.java
@@ -1,12 +1,11 @@
-package com.kspringall.pi4led;
+package com.kspringall.pi4led.controller;
+
+import com.kspringall.pi4led.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.groups.Default;
 
 @RestController
 public class GreetingController {
@@ -15,7 +14,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public Greeting greetingWorld(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
--- src/test/java/com/kspringall/test/SmokeTest.java
+++ src/test/java/com/kspringall/test/SmokeTest.java
@@ -4,6 +4,8 @@ package com.kspringall.test;
  * Created by kspringall on 1/24/2017.
  */
 
+import com.kspringall.pi4led.Application;
+import com.kspringall.pi4led.controller.GreetingController;
 import com.kspringall.pi4led.controller.LedController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,15 +16,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = LedController.class)
+@SpringBootTest(classes = Application.class)
 public class SmokeTest {
 
     @Autowired
-    private LedController controller;
+    private LedController ledController;
+
+    @Autowired
+    private GreetingController greetingController;
 
     @Test
-    public void contexLoads() throws Exception {
-        assertThat(controller).isNotNull();
+    public void contextLedLoads() throws Exception {
+        assertThat(ledController).isNotNull();
+    }
+
+    @Test
+    public void contextGreetingLoads() throws Exception {
+        assertThat(greetingController).isNotNull();
     }
 
 }
