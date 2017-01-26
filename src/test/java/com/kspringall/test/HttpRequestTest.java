935820e02c96baff8cc337fb1a3e90b96beb7846
kspringall <kimballg@springall.name>
2017-01-26 13:29:54 -0700

    Added @Before setup() to HttpRequestTest test

--- src/test/java/com/kspringall/test/HttpRequestTest.java
+++ src/test/java/com/kspringall/test/HttpRequestTest.java
@@ -3,7 +3,9 @@ package com.kspringall.test;
 /**
  * Created by kspringall on 1/24/2017.
  */
+
 import com.kspringall.pi4led.Application;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +15,15 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.net.URL;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = Application.class)
 public class HttpRequestTest {
+
+    private URL url;
 
     @LocalServerPort
     private int port;
@@ -25,9 +31,14 @@ public class HttpRequestTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Before
+    public void setUp() throws Exception {
+        this.url = new URL( "http://localhost:" + port + "/" );
+    }
+
     @Test
     public void greetingShouldReturnDefaultMessage() throws Exception {
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+        assertThat(this.restTemplate.getForObject( url.toString(),
                 String.class)).contains("Hello World");
     }
 }
